e6942a487653941cccbb0427724cce81886dc16f
xiaohui <xiaohui@zhangxh.net>
2015-12-17 10:46:21 +0800

    update xxxx
    add test comment

--- js/affix.js
+++ js/affix.js
@@ -5,6 +5,7 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
+ /*test*/
 
 
 +function ($) {
